7bdb6c27c4bb2d60b6396175ed1933f8e6324a3a
vivicoko <40648615+vivicoko@users.noreply.github.com>
2018-06-28 03:38:53 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -8,3 +8,37 @@ function makeGrid() {
 // Your code goes here!
 
 }
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+
+// Your code goes here!
+$('#sizePicker').submit(function (event){
+  event.preventDefault();
+  height = $('#inputHeight').val();
+  width = $('#inputWeight').val();
+  makeGride(height, width);
+
+});
+
+function makeGrid(num, y) {
+  
+  for (var x=1; x<=num; x++){
+  $('#pixelCanvas').append('<td></td>');
+  }
+}
+$('td').click(function addColor(){
+  color = $('#colorPicker').val();
+  if ($(this).attr('style')){
+  $(this).removeAttr('style')
+  } else{
+    $(this).attr('style', 'background-color:' + color);
+               
+    
+                 }
+  }
+
+});
+ 
