e3e3c18182aa828301211be9e8d2274a1840a654
vivicoko <40648615+vivicoko@users.noreply.github.com>
2018-06-28 03:51:22 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,5 +6,31 @@
 function makeGrid() {
 
 // Your code goes here!
+$('#sizePicker').submit(function(event) {
+  event.preventDefault();
+  height = $('#inputHeight').val();
+  width = $('#inputWeight').val();
+  makeGride(height, width);
+
+});
+
+function makeGrid(num, y) {
+  
+  for (var x=1; x<=num; x++){
+  $('#pixelCanvas').append('<td></td>');
+  }
+}
+$('td').click(function addColor(){
+  color = $('#colorPicker').val();
+  if ($(this).attr('style')){
+  $(this).removeAttr('style')
+  } else{
+    $(this).attr('style', 'background-color:' + color);
+               
+    
+                 }
+  }
+
+});
 
 }
